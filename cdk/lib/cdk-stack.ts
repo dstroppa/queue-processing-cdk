96202fff151a0ee6c168f86e3fb1d8a85bb41429
Daniele Stroppa <stroppad@amazon.com>
2020-01-03 13:39:58 +0000

    Moves from SAM to CDK

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,6 +2,7 @@ import cdk = require('@aws-cdk/core');
 import lambda = require("@aws-cdk/aws-lambda");
 import event = require("@aws-cdk/aws-lambda-event-sources");
 import sqs = require("@aws-cdk/aws-sqs");
+//import iam = require("@aws-cdk/aws-iam");
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -13,6 +14,14 @@ export class CdkStack extends cdk.Stack {
       type: "String",
       description: "App Id"
     });
+
+    // const executionRole = new iam.Role(this, "sqsPayloadLoggerFunctionRole", {
+    //   assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    //   managedPolicies: [
+    //     iam.ManagedPolicy.fromAwsManagedPolicyName("AWSLambdaBasicExecutionRole"),
+    //     iam.ManagedPolicy.fromAwsManagedPolicyName("AWSLambdaSQSQueueExecutionRole")
+    //   ]
+    // });
 
     // This is an SQS queue with all default configuration properties. To learn more about the available options, see
     // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-sqs-queues.html
@@ -27,10 +36,11 @@ export class CdkStack extends cdk.Stack {
       // Using inline code as a workaround as local assets are not supported yet, see https://github.com/aws/aws-cdk/issues/1312
       //code: lambda.Code.asset("../src/handlers"),
       code: lambda.Code.inline(this.localAsset(path.join(__dirname, "../../src/handlers/sqs-payload-logger.js"))),
-      timeout: cdk.Duration.seconds(25) //Chosen to be less than the default SQS Visibility Timeout of 30 seconds
+      timeout: cdk.Duration.seconds(25), //Chosen to be less than the default SQS Visibility Timeout of 30 seconds
+      //role: executionRole
     });
     // Give Read Permissions to the SQS queue
-    queue.grantConsumeMessages(sqsPayloadLoggerFunction);
+    //queue.grantConsumeMessages(sqsPayloadLoggerFunction);
     // Add event source
     sqsPayloadLoggerFunction.addEventSource(new event.SqsEventSource(queue));
 
